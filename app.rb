2e24785763cbae7d4d1333aa714fe0935311464e
ocawthorne <ocawthorne@gmail.com>
2020-11-11 16:04:49 +0000

    sinatra-nested-forms-111120

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
-# require '../../config/environment'
 require 'sinatra'
 
 class App < Sinatra::Base
 
-  set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "app/views/") }
 
   get '/' do
     erb :super_hero
